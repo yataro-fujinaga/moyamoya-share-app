ea58e939fa0ea1096daa6c5c3f935b0faeb904b1
madomaru <melondoka1502@gmail.com>
2022-07-05 11:34:32 +0900

    一時的

---
+++ pages/student.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+
+    }
+  }
+  methods: {
+    // postFirebase: async (formObj) => {
+    //   const database = this.$fire.firestore
+    //   try {
+    //     await database.addDoc(
+    //       database.collection(database, 'stamps', {
+    //         id: formObj.id,
+    //         image_url: formObj.image_url,
+    //         description: formObj.description,
+    //       })
+    //     )
+    //   } catch (error) {
+    //     console.log(error)
+    //   }
+    // },
+  }
+  
+}
+
+
+</script>
