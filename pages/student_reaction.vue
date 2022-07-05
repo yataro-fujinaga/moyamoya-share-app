5ccdd3b83083a7f0d0969b6853dd5a49dfbcf320
madomaru <melondoka1502@gmail.com>
2022-07-05 12:25:19 +0900

    あとはfirebaseの設定をすれば、postのtestできる

---
+++ pages/student_reaction.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        test
+        <button @click="postFirebase">post</button>   
+    </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+
+    }
+  },
+  methods: {
+    postFirebase: async (formObj,reactionType) => {
+      const database = this.$fire.firestore
+      try {
+        await database.addDoc(
+          database.collection(database, 'lectures/reactions/JngtPpv4ZQ9VwPUIca3C/reactions', {
+            ts: Date.now(),
+            type: "good",
+            uid: "test",
+          })
+        )
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    // beforePost(){
+
+    // }
+    
+  }
+  
+}
+
+
+</script>
