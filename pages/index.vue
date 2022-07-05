c289b92056b7cf22700ca8bc625d634688a278e8
Yataro Fujinaga <yatarofujinaga@gmail.com>
2022-07-05 12:35:56 +0900

    講義フォームページの作成

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,9 @@
 v-container
   v-row
     v-col(cols='12', xs='12', sm='12', md='12')
+      p あなたはどちらですか
+      v-btn(to='/lectures/create') 先生
+      v-btn(to='/lectures') 学生
 </template>
 
 <script>
