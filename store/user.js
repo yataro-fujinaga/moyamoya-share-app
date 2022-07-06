ebc0c5844e00f3e6c45e2a465ab06c0e0108b6e9
madomaru <melondoka1502@gmail.com>
2022-07-06 18:06:57 +0900

    storeの使わない値を消した

--- store/user.js
+++ store/user.js
@@ -1,12 +1,8 @@
 export const state = () => ({
-  id: 0,
   type: "",
 });
 
 export const mutations = {
-  setId (state, newId) {
-    state.id = newId
-  },
   setType(state,newType) {
     state.type = newType
   },
