dbe640b792ceb100583f3c9f8dff0816101b1469
Yataro Fujinaga <yatarofujinaga@gmail.com>
2022-07-07 23:05:21 +0900

    pug記法に対応

--- pages/index.vue
+++ pages/index.vue
@@ -4,12 +4,14 @@ v-container
     v-col(cols='12', xs='12', sm='12', md='12')
       p.text-center.font-weight-bold あなたはどちらですか
       .text-center
-        v-btn.pa-2.mr-3(depressed, color='teal')(
+        v-btn.pa-2.mr-3(
+          depressed,
+          color='teal',
           @click='setUserType("teacher")'
         )
           v-icon.white--text mdi-human-male-board
           span.white--text.font-weight-bold 先生
-        v-btn.pa-2(depressed, color='teal')(@click='setUserType("student")')
+        v-btn.pa-2(depressed, color='teal', @click='setUserType("student")')
           v-icon.white--text mdi-account-school-outline
           span.white--text.font-weight-bold 学生
 </template>
