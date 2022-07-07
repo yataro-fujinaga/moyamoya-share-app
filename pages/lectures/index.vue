6a2979168c6571df797946b3099c6c626a925422
Yataro Fujinaga <yatarofujinaga@gmail.com>
2022-07-07 14:26:31 +0900

    講義作成画面への遷移ボタンの追加

--- pages/lectures/index.vue
+++ pages/lectures/index.vue
@@ -2,14 +2,26 @@
 v-container
   v-row
     v-col(cols='12', xs='12', sm='12', md='12')
-      v-card(v-for="lecture in lectures" v-bind:key="lecture.id" @click="ToLecture(lecture.id)" )
-          v-card-title {{ lecture.data.name }}
-          v-card-subtitle {{ lecture.data.teacher_name }} 先生
+      .add-lecture-button.mb-2.text-right(
+        v-if='$store.state.user.type === "teacher"'
+      )
+        v-btn(color='red lighten-1', depressed, to='/lectures/create')
+          v-icon.white--text mdi-plus
+      v-card.mb-2(
+        v-for='lecture in lectures',
+        v-bind:key='lecture.id',
+        @click='ToLecture(lecture.id)'
+      )
+        v-card-title {{ lecture.data.name }}
+        v-card-subtitle
+          span 担当: {{ lecture.data.teacher_name }} 先生
 </template>
 
 <script>
+import LectureCard from '~/components/LectureCard.vue'
 export default {
   name: 'LectureIndexPage',
+  components: { LectureCard },
   data() {
     return {
       lectures: [],
@@ -32,20 +44,19 @@ export default {
       })
   },
   methods: {
-    ToLecture(id){
+    ToLecture(id) {
       const userType = this.$store.state.user.type
-      switch(userType){
-        case "student":
-          this.$router.push('/lectures/student/'+id)
-          break;
-        case "teacher":
-          this.$router.push('/lectures/teacher/'+id)
-          break;
+      switch (userType) {
+        case 'student':
+          this.$router.push('/lectures/student/' + id)
+          break
+        case 'teacher':
+          this.$router.push('/lectures/teacher/' + id)
+          break
         default:
           this.$router.push('/')
       }
-      
-    }
+    },
   },
 }
 </script>
