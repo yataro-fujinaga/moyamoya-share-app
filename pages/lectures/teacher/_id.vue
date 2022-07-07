f12273dd199b5d1416ca2cbe0118e765c52e52c9
Yataro Fujinaga <yatarofujinaga@gmail.com>
2022-07-07 14:27:06 +0900

    教員のリアクション表示画面を作成

--- pages/lectures/teacher/_id.vue
+++ pages/lectures/teacher/_id.vue
@@ -1,49 +1,54 @@
 <template lang="pug">
 v-container
-    v-card-title {{ lecture.name }}
-    v-card-subtitle {{ lecture.teacher_name }} 先生
+  v-row
+  v-col(cols='12', xs='12', sm='12', md='12')
+    BackButton(toUrl='/lectures')
+    v-card.mb-2
+      LectureCard(:lecture='lecture')
     h1 リアクション
-    // それぞれのリアクションを表示する
-    v-card(v-for="reaction in reactions" v-bind:key="reaction.id")
-       p {{ reaction.data.ts }}
-       p {{ reaction.data.type }}
+    v-card.mb-2(v-for='reaction in reactions', v-bind:key='reaction.id')
+      v-card-text
+        p {{ reaction.data.ts }}
+        p {{ reaction.data.type }}
 </template>
 
 <script>
+import LectureCard from '~/components/LectureCard.vue'
 // teacher
 export default {
-    name: 'LecturePage',
-    data() {
-        return {
-            lecture: {},
-            reactions: [],
-        }
-    },
-    head() {
-        return {
-            title: this.lecture.name,
-        }
-    },
-    mounted() {
-        const database = this.$fire.firestore
-        this.lecture = database
-            .collection('lectures')
-            .doc(this.$route.params.id)
-            .get()
-            .then((doc) => {
-                this.lecture = doc.data()
-            })
-        database
-            .collection('lectures')
-            .doc(this.$route.params.id)
-            .collection('reactions')
-            .get()
-            .then((querySnapshot) => {
-                querySnapshot.forEach((doc) => {
-                    this.reactions.push({ id: doc.id, data: doc.data() })
-                })
-            })
-    },
-    methods: {},
+  name: 'LecturePage',
+  components: { LectureCard },
+  data() {
+    return {
+      lecture: {},
+      reactions: [],
+    }
+  },
+  head() {
+    return {
+      title: this.lecture.name,
+    }
+  },
+  mounted() {
+    const database = this.$fire.firestore
+    this.lecture = database
+      .collection('lectures')
+      .doc(this.$route.params.id)
+      .get()
+      .then((doc) => {
+        this.lecture = doc.data()
+      })
+    database
+      .collection('lectures')
+      .doc(this.$route.params.id)
+      .collection('reactions')
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          this.reactions.push({ id: doc.id, data: doc.data() })
+        })
+      })
+  },
+  methods: {},
 }
 </script>
