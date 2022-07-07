cc435f74e14ba384e8510f2f94904e104d50ac81
Yataro Fujinaga <yatarofujinaga@gmail.com>
2022-07-07 14:26:54 +0900

    学生のリアクション作成画面の作成

--- pages/lectures/student/_id.vue
+++ pages/lectures/student/_id.vue
@@ -1,48 +1,55 @@
 <template lang="pug">
 v-container
-    v-card-title {{ lecture.name }}
-    v-card-subtitle {{ lecture.teacher_name }} 先生
-    <button @click="postFirebase('good')">good</button>  
-    <button @click="postFirebase('bad')">bad</button> 
-    
+  v-row
+    v-col(cols='12', xs='12', sm='12', md='12')
+      BackButton(toUrl='/lectures')
+      v-card.mb-2
+        LectureCard(:lecture='lecture')
+      .reaction-button.d-flex
+        v-col.text-center(cols='6', xs='6', sm='6', md='6')
+          v-btn(@click='postFirebase("good")', fab, color='primary') 
+            v-icon mdi-thumb-up
+        v-col.text-center(cols='6', xs='6', sm='6', md='6')
+          v-btn(@click='postFirebase("bad")', fab, color='error') 
+            v-icon mdi-thumb-down
 </template>
 
 <script>
-// student
+import LectureCard from '~/components/LectureCard.vue'
 export default {
-    name: 'LecturePage',
-    data() {
-        return {
-            lecture: {},
-            reactions: [],
-        }
-    },
-    head() {
-        return {
-            title: this.lecture.name,
-        }
-    },
-    mounted() {
-        const database = this.$fire.firestore
-        this.lecture = database
-            .collection('lectures')
-            .doc(this.$route.params.id)
-            .get()
-            .then((doc) => {
-                this.lecture = doc.data()
-            })
-    },
-    methods: {
-     postFirebase(reactionType){ 
+  name: 'LecturePage',
+  components: { LectureCard },
+  data() {
+    return {
+      lecture: {},
+      reactions: [],
+    }
+  },
+  head() {
+    return {
+      title: this.lecture.name,
+    }
+  },
+  mounted() {
+    const database = this.$fire.firestore
+    this.lecture = database
+      .collection('lectures')
+      .doc(this.$route.params.id)
+      .get()
+      .then((doc) => {
+        this.lecture = doc.data()
+      })
+  },
+  methods: {
+    postFirebase(reactionType) {
       const database = this.$fire.firestore
       database
-        .collection('lectures/'+this.$route.params.id+'/reactions')
+        .collection('lectures/' + this.$route.params.id + '/reactions')
         .add({
-            ts: new Date(),
-            type: reactionType,
-        });
-      }
-    
+          ts: new Date(),
+          type: reactionType,
+        })
     },
+  },
 }
 </script>
